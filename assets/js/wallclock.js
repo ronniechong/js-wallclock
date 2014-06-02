def7d13b4c439a2db9ddf652fc05ce0774ba9e0c
Ronnie Chong <ronnie@otakuness.com>
2014-06-02 22:28:25 +1000

    Converted to OOP

--- assets/js/wallclock.js
+++ assets/js/wallclock.js
@@ -1,1 +1,64 @@
-function init(){if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){document.getElementById("more").innerHTML="Device: "+navigator.userAgent;{setInterval(fnUpdateTime,1e3)}fnMotionDevice()}else document.getElementById("more").innerHTML="You need a compatible mobile device to test this page"}function fnMotionDevice(){window.DeviceOrientationEvent?(document.getElementById("type").innerHTML="Method: DeviceOrientationEvent",window.addEventListener("deviceorientation",function(){fnDetechTilt([event.beta,event.gamma])},!0)):window.DeviceMotionEvent?(document.getElementById("type").innerHTML="Method: DeviceMotionEvent",window.addEventListener("devicemotion",function(){fnDetechTilt([2*event.acceleration.x,2*event.acceleration.y])},!0)):window.addEventListener("MozOrientation",function(){fnDetechTilt([50*orientation.x,50*orientation.y])},!0)}function fnDetechTilt(e,t){"use strict";if("object"==typeof e){document.getElementById("output").innerHTML="Tilt: Y="+Math.round(e[0])+", X="+Math.round(e[1]);var n=Math.round(e[0]),i=Math.round(e[1]);document.getElementById("wallclock").style.webkitTransform="rotateY("+i+"deg) rotateX("+n+"deg)",document.getElementById("wallclock").style.transform="rotateY("+i+"deg) rotateX("+n+"deg)"}else document.getElementById("output").innerHTML=e+","+t}function fnUpdateTime(){"use strict";var e=new Date,t=e.getHours()>12?e.getHours()-12:e.getHours()<10?"0"+e.getHours():e.getHours(),n=e.getMinutes()<10?"0"+e.getMinutes():e.getMinutes(),i=e.getSeconds()<10?"0"+e.getSeconds():e.getSeconds(),o=e.getHours()>=12?"pm":"am";document.getElementsByClassName("hour")[0].innerHTML=t,document.getElementsByClassName("min")[0].innerHTML=n,document.getElementsByClassName("sec")[0].innerHTML=i,document.getElementsByClassName("day-night")[0].innerHTML=o}window.onload=function(){init()};
+
+function wallClock(){
+	
+
+	//Properties
+
+	this.version = '1.0';
+
+	//Methods
+	this.init = function(){
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ 			var clockTimer = setInterval(this.fnUpdateTime, 1000);	
+			document.getElementById('wallclock').style.display = "block";
+			document.getElementById('info').style.display = "none";
+			this.fnMotionDevice();
+		}
+	}
+
+	this.fnUpdateTime = function(){
+		'use strict';
+		var currDate = new Date(),
+			currHour = (currDate.getHours()>12)? currDate.getHours() - 12: (currDate.getHours()<10)? '0'+ currDate.getHours():currDate.getHours(),
+			currMin = (currDate.getMinutes()<10)?'0'+currDate.getMinutes():currDate.getMinutes(),
+			currSec = (currDate.getSeconds()<10)?'0'+currDate.getSeconds():currDate.getSeconds(),
+			dayNight =(currDate.getHours()>=12)?'pm':'am';
+
+		document.getElementsByClassName('hour')[0].innerHTML = currHour;
+		document.getElementsByClassName('min')[0].innerHTML = currMin;
+		document.getElementsByClassName('sec')[0].innerHTML = currSec;
+		document.getElementsByClassName('day-night')[0].innerHTML = dayNight;
+	}
+
+	this.fnMotionDevice = function(){
+		var that = this;
+		if (window.DeviceOrientationEvent) {
+		    window.addEventListener("deviceorientation", function () {
+		        that.fnDetectTilt([event.beta, event.gamma]);
+		    }, true);
+		} else if (window.DeviceMotionEvent) {
+		    window.addEventListener('devicemotion', function () {
+		        that.fnDetectTilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+		    }, true);
+		} else {
+		    window.addEventListener("MozOrientation", function () {
+		        that.fnDetectTilt([orientation.x * 50, orientation.y * 50]);
+		    }, true);
+		}
+
+	}
+	this.fnDetectTilt = function(param1, param2){
+		'use strict';
+		if (typeof(param1)==='object'){
+			document.getElementById('output').innerHTML = 'Tilt: Y='+Math.round(param1[0]) + ', X=' + Math.round(param1[1]);
+			var rotX = Math.round(param1[0]),
+				rotY = Math.round(param1[1]);
+			document.getElementById('wallclock').style.webkitTransform  = 'rotateY('+rotY+'deg) rotateX('+rotX+'deg)';
+			document.getElementById('wallclock').style.transform  = 'rotateY('+rotY+'deg) rotateX('+rotX+'deg)';
+			//document.getElementById('wallclock').style.webkitTransform  = 'rotateY('+Math.round((param1[1])*-1)+'deg)';
+			
+		}else{
+			document.getElementById('output').innerHTML = param1 + ',' + param2;
+		}
+	}
+}
--- src/js/wallclock.js
+++ src/js/wallclock.js
@@ -1,62 +1,64 @@
 
-window.onload = function(e){ 
-	init();
-}
-
-function init(){
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- 		document.getElementById('more').innerHTML = 'Device: ' + navigator.userAgent;
-		var clockTimer = setInterval(fnUpdateTime, 1000);	
-		fnMotionDevice();
-	}else{
-		document.getElementById('more').innerHTML = 'You need a compatible mobile device to test this page';
-	}
+function wallClock(){
 	
-}
 
-function fnMotionDevice(){
-	if (window.DeviceOrientationEvent) {
-		document.getElementById('type').innerHTML = 'Method: DeviceOrientationEvent';
-	    window.addEventListener("deviceorientation", function () {
-	        fnDetechTilt([event.beta, event.gamma]);
-	    }, true);
-	} else if (window.DeviceMotionEvent) {
-		document.getElementById('type').innerHTML = 'Method: DeviceMotionEvent';
-	    window.addEventListener('devicemotion', function () {
-	        fnDetechTilt([event.acceleration.x * 2, event.acceleration.y * 2]);
-	    }, true);
-	} else {
-	    window.addEventListener("MozOrientation", function () {
-	        fnDetechTilt([orientation.x * 50, orientation.y * 50]);
-	    }, true);
+	//Properties
+
+	this.version = '1.0';
+
+	//Methods
+	this.init = function(){
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ 			var clockTimer = setInterval(this.fnUpdateTime, 1000);	
+			document.getElementById('wallclock').style.display = "block";
+			document.getElementById('info').style.display = "none";
+			this.fnMotionDevice();
+		}
 	}
-}
 
-function fnDetechTilt(param1, param2){
-	'use strict';
-	if (typeof(param1)==='object'){
-		document.getElementById('output').innerHTML = 'Tilt: Y='+Math.round(param1[0]) + ', X=' + Math.round(param1[1]);
-		var rotX = Math.round(param1[0]),
-			rotY = Math.round(param1[1]);
-		document.getElementById('wallclock').style.webkitTransform  = 'rotateY('+rotY+'deg) rotateX('+rotX+'deg)';
-		document.getElementById('wallclock').style.transform  = 'rotateY('+rotY+'deg) rotateX('+rotX+'deg)';
-		//document.getElementById('wallclock').style.webkitTransform  = 'rotateY('+Math.round((param1[1])*-1)+'deg)';
-		
-	}else{
-		document.getElementById('output').innerHTML = param1 + ',' + param2;
+	this.fnUpdateTime = function(){
+		'use strict';
+		var currDate = new Date(),
+			currHour = (currDate.getHours()>12)? currDate.getHours() - 12: (currDate.getHours()<10)? '0'+ currDate.getHours():currDate.getHours(),
+			currMin = (currDate.getMinutes()<10)?'0'+currDate.getMinutes():currDate.getMinutes(),
+			currSec = (currDate.getSeconds()<10)?'0'+currDate.getSeconds():currDate.getSeconds(),
+			dayNight =(currDate.getHours()>=12)?'pm':'am';
+
+		document.getElementsByClassName('hour')[0].innerHTML = currHour;
+		document.getElementsByClassName('min')[0].innerHTML = currMin;
+		document.getElementsByClassName('sec')[0].innerHTML = currSec;
+		document.getElementsByClassName('day-night')[0].innerHTML = dayNight;
 	}
-}
 
-function fnUpdateTime(){
-	'use strict';
-	var currDate = new Date(),
-		currHour = (currDate.getHours()>12)? currDate.getHours() - 12: (currDate.getHours()<10)? '0'+ currDate.getHours():currDate.getHours(),
-		currMin = (currDate.getMinutes()<10)?'0'+currDate.getMinutes():currDate.getMinutes(),
-		currSec = (currDate.getSeconds()<10)?'0'+currDate.getSeconds():currDate.getSeconds(),
-		dayNight =(currDate.getHours()>=12)?'pm':'am';
+	this.fnMotionDevice = function(){
+		var that = this;
+		if (window.DeviceOrientationEvent) {
+		    window.addEventListener("deviceorientation", function () {
+		        that.fnDetectTilt([event.beta, event.gamma]);
+		    }, true);
+		} else if (window.DeviceMotionEvent) {
+		    window.addEventListener('devicemotion', function () {
+		        that.fnDetectTilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+		    }, true);
+		} else {
+		    window.addEventListener("MozOrientation", function () {
+		        that.fnDetectTilt([orientation.x * 50, orientation.y * 50]);
+		    }, true);
+		}
 
-	document.getElementsByClassName('hour')[0].innerHTML = currHour;
-	document.getElementsByClassName('min')[0].innerHTML = currMin;
-	document.getElementsByClassName('sec')[0].innerHTML = currSec;
-	document.getElementsByClassName('day-night')[0].innerHTML = dayNight;
+	}
+	this.fnDetectTilt = function(param1, param2){
+		'use strict';
+		if (typeof(param1)==='object'){
+			document.getElementById('output').innerHTML = 'Tilt: Y='+Math.round(param1[0]) + ', X=' + Math.round(param1[1]);
+			var rotX = Math.round(param1[0]),
+				rotY = Math.round(param1[1]);
+			document.getElementById('wallclock').style.webkitTransform  = 'rotateY('+rotY+'deg) rotateX('+rotX+'deg)';
+			document.getElementById('wallclock').style.transform  = 'rotateY('+rotY+'deg) rotateX('+rotX+'deg)';
+			//document.getElementById('wallclock').style.webkitTransform  = 'rotateY('+Math.round((param1[1])*-1)+'deg)';
+			
+		}else{
+			document.getElementById('output').innerHTML = param1 + ',' + param2;
+		}
+	}
 }
